5264297e5c326c87892d9bb1ea7d9d1fa9c1556b
sehwanforeal <sehwanforeal@gmail.com>
2024-03-27 19:36:55 +0900

    add SubscriptionStore example

---
+++ src/SubscriptionStore/example.tsx
@@ -0,0 +1,33 @@
+import { createStore, useStore } from "@/SubscriptionStore/store";
+import { Box } from "@/Box";
+
+export const SubscriptionStoreExample = () => {
+  return (
+    <Box big>
+      SubscriptionStore
+      <Foo />
+      <Bar />
+    </Box>
+  );
+};
+
+const store = createStore({ count: 1 });
+
+const Foo = () => {
+  const [state, setState] = useStore(store);
+
+  return <Box>Foo: {state.count}</Box>;
+};
+
+const Bar = () => {
+  const [state, setState] = useStore(store);
+
+  return (
+    <Box>
+      Bar: {state.count}
+      <button onClick={() => setState({ count: state.count + 1 })}>
+        increase
+      </button>
+    </Box>
+  );
+};
